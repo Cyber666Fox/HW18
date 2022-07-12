4aacc482f29b02a279bb3e076279110e82a73c22
Cyber666Fox <104504035+Cyber666Fox@users.noreply.github.com>
2022-07-12 20:15:03 +0800

    fixed
    fixed

--- views/movies.py
+++ views/movies.py
@@ -14,7 +14,7 @@ class MoviesView(Resource):
         return movies, 200
 
     def post(self):
-        new_movie = movie_service.create_movie(request.json)
+        new_movie = movie_service.create_movies(request.json)
         resp = make_response("",201)
         resp.headers['location'] = f"{movie_ns.path}/{new_movie.id}"
         return resp
